da179632a43cc8caf7fb10bb7843f22b7e300927
EnyxRB <noahjjohnson22@gmail.com>
2018-02-20 22:35:32 +0000

    Updated with online server details and debugging section

--- website_18_02_18/includes/settings.php
+++ website_18_02_18/includes/settings.php
@@ -3,15 +3,34 @@
 
 $local = true; //Do you want to connect to local or online server?
 
-//Details for the online server
-$oservername = ""; //Name of the online server
-$odbname = ""; //Database on the server to enter
-$odbusername = ""; //Database login username
-$odbpassword = ""; //Database password username
+//Details for the online server (Don't change)
+$oservername = "csmysql.cs.cf.ac.uk"; //Name of the online server
+$odbname = "group8_2017"; //Database on the server to enter
+$odbusername = "group8.2017"; //Database login username
+$odbpassword = "kMB8PRzvnZf7Amg"; //Database password username
 
 //Details for the local server
-$lservername = "127.0.0.1:3306"; //Name of the local server
-$ldbname = "c1529854"; //Database on the server to enter
-$ldbusername = "root"; //Database login username
-$ldbpassword = ""; //Database password username	
+$lservername = "127.0.0.1:3306"; //Name of the local server (wamps standard)
+$ldbname = "group8_2017"; //Database on the server to enter 
+$ldbusername = "root"; //Database login username (wamp's standard is 'root')
+$ldbpassword = ""; //Database password username	(wamp's standard is nothing) 
+
+
+/*
+-----------------DEBUG SECTION-----------------
+Q: How can I recereate the database on a local server to test data
+without disrupting the online data?
+
+A: Install wamp server (https://www.youtube.com/watch?v=MsMaiHqkKkQ)
+then create a new database at localhost/phpmyadmin called 'group8_2017'.
+After this, import the dummy data called 'dummyDataFilledDatabase.sql'.
+Finally, change the $local variable to true and it should work. 
+
+
+Q: Set $local to true and cannot login in using voter login page?
+
+A: Check the capitalisation of your table's names, wamp uses lowercase
+tables names while the online version's start with a capital.
+-----------------------------------------------
+*/
 ?>
