6dc6d56ad1dd6a3cb337a093becb7c33c1784fdb
EnyxRB <noahjjohnson22@gmail.com>
2018-02-21 22:46:36 +0000

    Added log out for admin users

--- website_18_02_18/includes/logout.php
+++ website_18_02_18/includes/logout.php
@@ -4,19 +4,33 @@
 session_start(); 
 
 require_once('functions.php'); 
-include('settings.php');
 
 if (!isset($_SESSION['logged_in'])){ 
 	redirect('login.php'); 
 } 
 
-// Kill session variables 
+$admin = isset($_SESSION['admin']); //checking if user logging out is an admin
+
+//Kill neutral session variables
 unset($_SESSION['logged_in']); 
 unset($_SESSION['username']);
-unset($_SESSION['constituency']); 	
 
-// Destroy session 
+//Kill admin session variables
+if ($admin){ 
+	unset($_SESSION['admin']); 
+}  	
+
+//Kill voter session variables 
+else{
+	unset($_SESSION['constituency']);
+}
+
+//Destroy session 
 session_destroy();
+
+if ($admin){ 
+	redirect('../pages/admin-login.php'); //back to admin login	
+}
 
 redirect('../index.php'); //back to login			  
 
