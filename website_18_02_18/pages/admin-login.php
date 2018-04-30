361ba04868c9f8cfac5fecbc2f1a145b905d4adf
Derds <dervlaob@gmail.com>
2018-04-30 18:31:43 +0100

    added stying to php messages

--- website_18_02_18/pages/admin-login.php
+++ website_18_02_18/pages/admin-login.php
@@ -71,7 +71,45 @@ if(isset($_SESSION['logged_in'])){
 		
 		<?php
 			if (isset($_SESSION['error'])){
-				echo $_SESSION['error'];
+				echo "<!DOCTYPE html>
+                <html lang='en'>
+                <head>
+                    <meta charset='utf-8'>
+                    <title>Home Page</title>
+                    <!--For Bootstrap, to make page responsive on mobile-->
+                    <meta name='viewport' content='width=device-width, initial-scale=1'>
+                    <!--For Bootstrap, to load the css information from a CDN-->
+                    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+                    <link href='css/electago.css' rel='stylesheet' type='text/css'>
+                    <link href='https://fonts.googleapis.com/css?family=Montserrat|Open+Sans' rel='stylesheet'>
+                    <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+                    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
+                </head>
+                <body>
+                    <header class='container-fluid text-center'>
+                        <div id='logo'>
+                            <img src='images/logo.png' width='300' height='100' alt=''>
+                        </div>
+                    </header>
+                    <!--container class used in bootstrap to make a dynamic container of a fixed size-->
+                    <div class='container'> <p> " .$_SESSION['error']. "</p> </div>
+                    <footer class='container-fluid'>
+                    <!--info here: logo, copyright, links, login as admin-->
+
+                    <div id='small_logo' class='media'>
+                        <img src='images/small_logo.png' width='100' height='35' alt=''>
+                    </div>
+                    <div class='media-body'>
+                    <ul class='list-inline pull right'>
+                        <li><a href='#'>Help</a></li>
+                        <li><p>Other links</p></li>
+                        <li><a href='pages/index.php'>Back to voter login</a></li>
+                        <li><p> &copy; 2018, Group 8. All rights reserved.</p></li>
+                    </ul>
+                    </div>
+                </footer>
+                </body>
+                </html>";
 				unset($_SESSION['error']); 
 			}			
 		?>
