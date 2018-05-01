6d80df8e75ff1ccd9fe1e1767a8fbe02977d267f
Derds <dervlaob@gmail.com>
2018-05-01 21:25:06 +0100

    Login Pages (hopefully) completely fixed
    -fixed a broken link in the admin login
    -made the input field on the login (index) page become empty when you click on it again
    -hopefully fixed all of the bugs on both login pages (admin and voter)

--- website_18_02_18/pages/admin/index.php
+++ website_18_02_18/pages/admin/index.php
@@ -30,65 +30,61 @@ if(!isset($_SESSION['admin'])){
                             <img src='images/logo.png' width='300' height='100' alt=''>
                         </div>
         </header>
-		<h3>Welcome to electago Admin Home Page</h3>
+        <div class="container">
+		<div class="container-fluid col-sm-offset-2">
+			<h3>Welcome to electago Admin Home Page</h3>
 
-		<div class="card" style="width: 18rem;">
-		  <img class="card-img-top" src="..." alt="Card image cap">
-		  <div class="card-body">
-		    <h5 class="card-title">Card title</h5>
-		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-		    <a href="#" class="btn btn-primary">Go somewhere</a>
-		  </div>
+			<div class="card" style="width: 18rem;">
+			  <img class="card-img-top" src="..." alt="Card image cap">
+			  <div class="card-body">
+			    <h5 class="card-title">Create/View Elections</h5>
+			    <p class="card-text">Click here to create a new election or to view all current elections.</p>
+			    <a href="elections.php" class="btn btn-primary pull-right"><span class="glyphicon glyphicon-menu-right"></span>
+			    	<span class="glyphicon glyphicon-menu-right"></span></a>
+			  </div>
+			</div>
+
+			<div class="card" style="width: 18rem;">
+			  <img class="card-img-top" src="..." alt="Card image cap">
+			  <div class="card-body">
+			    <h5 class="card-title">View Election Results</h5>
+			    <p class="card-text">Click the arrows below.</p>
+			    <a href="elections.php" class="btn btn-primary pull-right"><span class="glyphicon glyphicon-menu-right"></span>
+			    	<span class="glyphicon glyphicon-menu-right"></span></a>
+			  </div>
+			</div>
+
+			<div>
+				<a href="elections.php"><button type="button" class="btn btn-secondary">Create/View Elections</button></a><p></p>
+				<a href="results.php"><button type="button" class="btn btn-secondary">View Election Results</button></a><p></p>
+				<a href="candidates.php"><button type="button" class="btn btn-secondary">Add/Edit Candidates</button></a><p></p>
+				<a href="uploadKey.php"><button type="button" class="btn btn-secondary">Upload key</button></a><p></p>
+				<a href="decryptScreen.php"><button type="button" class="btn btn-secondary">Decrypt Votes</button></a><p></p>
+				<a href="demographics.php"><button type="button" class="btn btn-secondary">View Demographics</button></a>
+			</div>
+
+
+					<div class="card" style="width: 18rem;">
+			  <img class="card-img-top" src="..." alt="Card image cap">
+			  <div class="card-body">
+			    <h5 class="card-title">Card title</h5>
+			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			    <a href="#" class="btn btn-primary">Go somewhere</a>
+			  </div>
+			</div>
+
+			<a href="../../includes/logout.php"><button type="button" class="btn btn-alert">Log out</button></a>
+			</div>
 		</div>
-		<a href="elections.php"><button type="button" class="btn btn-secondary">Create/View Elections</button></a><p><p>
-		<a href="results.php"><button type="button" class="btn btn-secondary">View Election Results</button></a><p><p>
-		<a href="candidates.php"><button type="button" class="btn btn-secondary">Add/Edit Candidates</button></a><p><p>
-		<a href="uploadKey.php"><button type="button" class="btn btn-secondary">Upload key</button></a><p></p>
-		<a href="decryptScreen.php"><button type="button" class="btn btn-secondary">Decrypt Votes</button></a><p></p>
-		<a href="demographics.php"><button type="button" class="btn btn-secondary">View Demographics</button></a>
-
-		<a href="../../includes/logout.php"><p>Log out</p></a>
-	</body>
-
-
-	<footer class="container-fluid text-left">
-		<!--info here: logo, copyright, links, login as admin-->
-		<ul>
-			<li><a href="#">Help</a></li>
-			<li><p>Other links</p></li>
-			<li><a href="../../includes/logout.php"><p>Log out</p></a></li>
-			<li><p> &copy; 2018, Group 8. All rights reserved.</p></li>
-		</ul>
-	</footer>
-=======
-
-		<div class="btn-group-vertical">
-			<a href="elections.php"><button type="button" class="btn btn-secondary">Create/View Elections</button></a><p><p>
-			<a href="results.php"><button type="button" class="btn btn-secondary">View Election Results</button></a><p><p>
-			<a href="candidates.php"><button type="button" class="btn btn-secondary">Add/Edit Candidates</button></a><p><p>
-			<a href="uploadKey.php"><button type="button" class="btn btn-secondary">Upload key</button></a><p></p>
-			<a href="decryptVotes.php"><button type="button" class="btn btn-secondary">Decrypt Votes</button></a><p></p>
-			<a href="demographics.php"><button type="button" class="btn btn-secondary">View Demographics</button></a>
-		</div> 
-
-		<a href="../../includes/logout.php"><button type="button" class="btn btn-alert">Log out</button></a>
-
-
-	<footer class='container-fluid'>
-        <!--info here: logo, copyright, links, login as admin-->
-
-        <div id='small_logo' class='media'>
-            <img src='images/small_logo.png' width='100' height='35' alt=''>
-        </div>
-        <div class='media-body'>
-        <ul class='list-inline pull right'>
-            <li><a href='#'>Help</a></li>
-            <li><p>Other links</p></li>
-            <li><a href='pages/index.php'>Back to voter login</a></li>
-            <li><p> &copy; 2018, Group 8. All rights reserved.</p></li>
-        </ul>
-        </div>
-    </footer>
-    	</body>
->>>>>>> e4f07d613128ba55ec0fd5127d2e193b6e2a41f8
+		<footer class="container-fluid text-left">
+			<!--info here: logo, copyright, links, login as admin-->
+			<ul>
+				<li><a href="#">Help</a></li>
+				<li><p>Other links</p></li>
+				<!--<span class="glyphicon glyphicon-log-out"></span><a href="../../includes/logout.php"><p>Log out</p></a>-->
+				<li><a href="../../includes/logout.php"><p>Log out</p></a></li>
+				<li><p> &copy; 2018, Group 8. All rights reserved.</p></li>
+			</ul>
+		</footer>
+    </body>
 </html>
